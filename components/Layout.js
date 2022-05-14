f02f513d020c4a1a798f7eaa112ffcf2e8566857
Pratik Sharma <sharma.pratik2016@gmail.com>
2022-05-14 14:48:37 +0530

    new changes

---
+++ components/Layout.js
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import Image from "next/image";
+function Layout({ children }) {
+  return (
+    <>
+      <header>
+        <nav>
+          <Link href="/" passHref>
+            <li style={{ display: "flex" }}>
+              <div className="logo"> Donate Via UPI</div>
+            </li>
+          </Link>
+        </nav>
+      </header>
+      <section>{children}</section>
+
+      <footer className="footer responsive">
+        <div className="flex column responsive" style={{ gap: "20px" }}>
+          <a
+            href="https://twitter.com/biomathcode"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Created by
+            {"\u2002Pratik Sharma "}
+          </a>
+          <a
+            href="https://github.com/pratiksharm/pay-via-upi"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Visit Github Repo
+          </a>
+        </div>
+        <div className="flex center">© Copyright 2022</div>
+      </footer>
+    </>
+  );
+}
+
+export default Layout;
